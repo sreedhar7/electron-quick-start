01e6841f8c916d5c434bb4df77dfb39b98bbe1f7
Sreedhar Veeravalli <srveerav@microsoft.com>
2020-06-29 12:32:19 -0700

    Add sw and renderer processes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let tmpWindows = []
 
 function createWindow () {
   // Create the browser window.
@@ -19,8 +20,32 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
+  const numWindows = 5;
+  for(let i = 0; i < numWindows; i++) {
+    // Create the browser window.
+    tmpWindows[i] = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        webviewTag: true
+      }
+    })
+
+    // and load the index.html of the app.
+    tmpWindows[i].loadFile('index.html')
+    tmpWindows[i].webContents.openDevTools()
+    // Emitted when the window is closed.
+    tmpWindows[i].on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      tmpWindows[i] = null
+    })    
+  }
+  
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -29,6 +54,7 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
 }
 
 // This method will be called when Electron has finished
